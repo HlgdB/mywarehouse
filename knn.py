438481d425a73a234094c5266eb97232a3bc96ec
HlgdB <1297693955@qq.com>
2019-04-07 16:55:52 +0800

    添加注释

--- knn.py
+++ knn.py
@@ -6,11 +6,11 @@ min_max = preprocessing.MaxAbsScaler()
 
 data = pd.read_csv('diabetes.csv')
 
-def creatdata():
+def creatdata():                  #数据初始化
     data_1 = data[:700]
-    train_data = data_1.drop('Outcome',axis = 1)
+    train_data = data_1.drop('Outcome',axis = 1)    
     train_array = np.array(train_data)
-    train_array = min_max.fit_transform(train_array)
+    train_array = min_max.fit_transform(train_array)    #将值都集中到0到1区间中，减少误差
     train_outcome = data_1.pop('Outcome')
     outcome_array = np.array(train_outcome)
 
@@ -18,24 +18,24 @@ def creatdata():
     labels = outcome_array 
     return group,labels
 
-def classify(input,data,label,k):
-    datasize = data.shape[0]
-    diff = np.tile(input,(datasize,1)) - data
+def classify(input,data,label,k):                  #knn算法具体实现
+    datasize = data.shape[0]                       #计算训练集行长度
+    diff = np.tile(input,(datasize,1)) - data      #计算预测目标与训练集个组数据的元素差
     sqdiff = diff ** 2
-    squaredist = np.sum(sqdiff,axis = 1)
+    squaredist = np.sum(sqdiff,axis = 1)           #计算每一行的和
     dist= squaredist ** 0.5
 
-    sortdist = np.argsort(dist)
+    sortdist = np.argsort(dist)                    #按从小到大排序
 
-    classcount = {}
+    classcount = {}                       
 
-    for i in range(k):
-        votel = label[sortdist[i]]
+    for i in range(k): 
+        votel = label[sortdist[i]]                 #用label产生新的键
 
-        classcount[votel] = classcount.get(votel,0) + 1
+        classcount[votel] = classcount.get(votel,0) + 1    #get的作用：若字典中有votel存在，则返回其值；若没有则初始一个并且初始值设置为0
 
     maxcount = 0
-    for key,value in classcount.items():
+    for key,value in classcount.items():           #遍历字典查询最大值对应的种类
         if value > maxcount:
             maxcount = value
             classes = key
@@ -44,7 +44,7 @@ def classify(input,data,label,k):
 
 dataset,labels = creatdata()
 
-data_2 = data[700:768]
+data_2 = data[700:768]                  #初始化测试集
 test_data = data_2.drop('Outcome',axis = 1)
 test_array = np.array(test_data)
 test_array = min_max.fit_transform(test_array)
@@ -57,9 +57,9 @@ true_array = np.array(true_output)
 maxcorrect = 0
 maxj = 0
 
-for j in range(50,200):
+for j in range(50,200):                #设置循环寻找最优K值
 
-    for i in range(68):
+    for i in range(68):           
         input = test_array[i]
         k = j
         output.append(classify(input,dataset,labels,k))
@@ -74,7 +74,7 @@ for j in range(50,200):
     correct = st/68
     #print(correct)
 
-    del output[:]    
+    del output[:]                     #每循环一次要将列表清零不然会导致列表越来越长
 
     if correct > maxcorrect:
         maxcorrect = correct
